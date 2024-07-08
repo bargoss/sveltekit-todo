a57122e09f5155cf7b339ba65bcffe10d30103eb
bargos <yesiltepebaran98@gmail.com>
2024-07-08 13:51:40 -0700

    also do the proxy for build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-// export default defineConfig({
-// 	plugins: [sveltekit()]
-// });
-
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
+		proxy: {
+			'/api': {
+				target: 'https://baransapp2-f9c9gyebftbseyb7.eastus-01.azurewebsites.net/api/v1',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	},
+	build: {
 		proxy: {
 			'/api': {
 				target: 'https://baransapp2-f9c9gyebftbseyb7.eastus-01.azurewebsites.net/api/v1',
